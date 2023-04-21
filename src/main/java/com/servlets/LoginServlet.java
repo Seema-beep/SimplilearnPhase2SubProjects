08c98e0c8ff94e6d3eb2419158165aa67615e43f
Seema-beep <seemabhuyan2020@gmail.com>
2023-04-21 10:37:25 +0530

    SecondPhaseSubProject1

---
+++ src/main/java/com/servlets/LoginServlet.java
@@ -0,0 +1,44 @@
+package com.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		if (username.equals("seema") && password.equals("admin")) {
+			response.sendRedirect("dashboard.html");
+			//out.print("Welcome, "+username);  
+	       // HttpSession session=request.getSession();  
+	       // session.setAttribute("name",username); 
+		} else {
+
+			 response.sendRedirect("error.jsp");
+		}
+		
+		out.close();
+	}
+
+}
---
+++ src/main/webapp/error.jsp
@@ -0,0 +1,19 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login Error</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        
+        
+        
+    </head>
+    <body>
+    <div class="error-container">
+		<h1>Oops! Something went wrong.</h1>
+		<p>We're sorry, but Invalid UserName or Password.</p>
+		<p>Please try again later, with correct credentials</p>
+		<a href="login.html" class="button">Go back to homepage</a>
+	</div>
+    </body>
+</html>
